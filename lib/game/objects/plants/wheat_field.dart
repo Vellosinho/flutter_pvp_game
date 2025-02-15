5905bd5f261d17adb89bf5777a8de0fe883e3db5
Lucas Velloso <lucas.velloso@dilettasolutions.com>
2025-02-15 13:19:23 -0300

    created flamable mixin

--- lib/game/objects/plants/wheat_field.dart
+++ lib/game/objects/plants/wheat_field.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:bonfire/bonfire.dart';
+import 'package:bonfire/mixins/flamable.dart';
 import 'package:flutter/material.dart';
 import 'package:projeto_gbb_demo/common/common.dart';
 import 'package:projeto_gbb_demo/game.dart';
@@ -8,7 +9,7 @@ import 'package:projeto_gbb_demo/game/controller/game_controller.dart';
 import 'package:projeto_gbb_demo/game/npcs/static_dummy.dart';
 import 'package:projeto_gbb_demo/game/objects/object_sprites.dart';
 
-class Wheat extends GameDecoration with Attackable {
+class Wheat extends GameDecoration with Attackable, Flamable{
   bool isOnFire = false;
   int spreadCount = 10;
   Random rand = Random();
@@ -35,13 +36,14 @@ class Wheat extends GameDecoration with Attackable {
     void onReceiveDamage(attacker, damage, identify, damageType) {
       if(damageType == DamageType.FIRE && spreadCount > 0) {
         int chance = rand.nextInt(10);
-        if (chance > 5) {
+        if (chance > 4) {
           setFire();
         }
       }
       super.onReceiveDamage(attacker, damage, identify, damageType);
     }
 
+    @override
     Future<void> setFire() async {
       int animationNum = rand.nextInt(2);
       if (animationNum < 1) {
@@ -49,59 +51,13 @@ class Wheat extends GameDecoration with Attackable {
       } else {
         setAnimation(await GameObjectsSprites.wheatFire2);
       }
-      setupLighting(
-      LightingConfig(
-        radius: width * 1.2,
-        color: Color(0xffea5c0a).withAlpha(80),
-        // color: Color(0xff9dc1e8).withAlpha(80),
-        blurBorder: 160,
-        align: Vector2(0, 128),
-        ),
-      );
-
-      Future.delayed(Duration(seconds: 5), () {
-        fireSpread();
-      });
+      super.setFire();
     }
 
-    void fireSpread() {
-      if(spreadCount > 0) {
-        int chance = rand.nextInt(10);
-
-        if (chance > 8) {
-          simpleAttackMeleeByAngle(
-            centerOffset: Vector2(-96, -96),
-            // sizePush: 0.2,
-            withPush: false,
-            damage: 5, 
-            angle: 0, 
-            attackFrom: AttackOriginEnum.WORLD,
-            size: Vector2(384, 384), 
-            damageType: DamageType.FIRE,
-          );
-        }
-        spreadCount--;
-
-
-        Future.delayed(Duration(seconds: 5), () {
-          fireSpread();
-        });
-      } else {
-        killWheat();
-      }
-    }
-
-    void killWheat() async {
-      setupLighting(
-        LightingConfig(
-          radius: 0,
-          color: Color(0xffea5c0a).withAlpha(80),
-          // color: Color(0xff9dc1e8).withAlpha(80),
-          blurBorder: 160,
-          align: Vector2(0, 128),
-          ),
-      );
+    @override
+    Future<void> onDie() async {
       setAnimation(await GameObjectsSprites.deadWheat);
+      super.onDie();
     }
         
 }
