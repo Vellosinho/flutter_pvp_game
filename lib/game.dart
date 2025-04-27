d8f52c26162585b21abceb8b309a91b2516ae481
Lucas Velloso <lucas.velloso@dilettasolutions.com>
2025-04-27 14:51:54 -0300

    criado o mapa da tavern

--- lib/game.dart
+++ lib/game.dart
@@ -5,6 +5,7 @@ import 'package:projeto_gbb_demo/game/npcs/npcs.dart';
 import 'package:projeto_gbb_demo/game/objects/daytime_clock.dart';
 import 'package:projeto_gbb_demo/game/objects/objects.dart';
 import 'package:projeto_gbb_demo/game/objects/plants/wheat_field.dart';
+import 'package:projeto_gbb_demo/maps/tavern.dart';
 import 'package:projeto_gbb_demo/parallax/parallax_clouds.dart';
 import 'package:projeto_gbb_demo/players/player_one/blacksmith/blacksmith.dart';
 import 'game/game_sprite_sheet.dart';
@@ -54,82 +55,18 @@ class _GameState extends State<Game> {
   Widget build(BuildContext context) {
     LocalGameController gameController = context.read<LocalGameController>();
 
-    return BonfireWidget(
-      backgroundColor: Color(0xff2c6ec7),
-      background: BonfireParallaxBackground(),
-      playerControllers: [
-        Keyboard(
-            config: KeyboardConfig(acceptedKeys: [
-          LogicalKeyboardKey.arrowDown,
-          LogicalKeyboardKey.arrowLeft,
-          LogicalKeyboardKey.arrowUp,
-          LogicalKeyboardKey.arrowRight,
-          LogicalKeyboardKey.keyZ,
-          LogicalKeyboardKey.keyX,
-          LogicalKeyboardKey.keyC,
-          LogicalKeyboardKey.escape,
-        ]))
-      ],
-      // gameController: gameController,
-      lightingColorGame: Colors.orange[400]!.withAlpha(48),
-      components: [
-        // BlackSmithMaster(
-        //     position: Vector2(tileSize * 19.25, tileSize * 15.5),
-        //     size: PlayerConsts.tallNPCSize,
-        //     hitboxSize: PlayerConsts.characterHitbox,
-        //     hitboxPosition: PlayerConsts.hitboxPosition,
-        //     controller: gameController),
-        // Anvil(
-        //     position: Vector2(tileSize * 21.5, tileSize * 19.5),
-        //     localGameController: gameController),
-        // Furnace(
-        //     position: Vector2(tileSize * 21, tileSize * 11),
-        //     localGameController: gameController),
-        // SwordShippingBox(
-        //     position: Vector2(tileSize * 19, tileSize * 18.5),
-        //     localGameController: gameController),
-        // LaunchStation(
-        //     position: Vector2(tileSize * 14, tileSize * 13.5),
-        //     localGameController: gameController),
-        // SmithingTable(
-        //     position: Vector2(tileSize * 22.75, tileSize * 16.85),
-        //     localGameController: gameController),
-        // DayTimeClock(
-        //     position: Vector2(0, 0), localGameController: gameController),
-        // ...wheatField!,
-      ],
-      // ],
-      cameraConfig: CameraConfig(zoom: 0.8, moveOnlyMapArea: true),
-      // cameraConfig: CameraConfig(zoom: 0.2),
-      map: WorldMapByTiled(
-          // WorldMapReader.fromAsset('map/new_map/ruins_map_pvp.json'),
-          WorldMapReader.fromAsset('map/house_interior/yellow_house/yellow_house_map.json'),
-          forceTileSize: Vector2(tileSize, tileSize)),
-      player: BlacksmithClass(
-        localGameController: gameController,
-        id: id,
-        playerLife: context.watch<LocalGameController>().playerLife.toDouble(),
-        onHit: () {
-          gameController.hit(2);
-        },
-        faction: playerFaction,
-        position: Vector2(tileSize * 15, tileSize * 13.5),
-      ),
-      overlayBuilderMap: {
-        PlayerInterface.overlayKey: (context, game) => PlayerInterface(
-            game: game,
-            characterClass: playerOneClass,
-            characterFaction: playerFaction),
-        // PauseMenu.overlayKey: (context,game) => gameController.gameIsPaused ? const PauseMenu() : const SizedBox(),
-        PauseMenu.overlayKey: (context, game) => PauseMenu(),
-        MiniGame.overlayKey: (context, game) => MiniGame(),
-      },
-      initialActiveOverlays: const [
-        PlayerInterface.overlayKey,
-        PauseMenu.overlayKey,
-        MiniGame.overlayKey,
-      ],
-      // showCollisionArea: true,
-    );
+    return TavernMap(
+        player: BlacksmithClass(
+          localGameController: gameController,
+          id: id,
+          playerLife:
+              context.watch<LocalGameController>().playerLife.toDouble(),
+          onHit: () {
+            gameController.hit(2);
+          },
+          faction: playerFaction,
+          position: Vector2(tileSize * 15, tileSize * 13.5),
+        ),
+        controller: gameController);
   }
 }
